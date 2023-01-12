bd2afe7b979ed9d135fbfb0bb91b70c603dacc2b
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-01-12 14:23:48 +0200

    username js update

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.addEventListener("load", () => {
     todo = JSON.parse(localStorage.getItem("todos")) || [];
-    
+    const nameInput = document.querySelector("#name");
+    const newToDoForm = document.querySelector("#new-to-do-form");
+
+    const username = localStorage.getItem("username") || "";
+
+    nameInput.value = username;
+
+    nameInput.addEventListener("change", e =>{
+        localStorage.setItem("username, e.target.value");
+    })
 })
